c955fba0fbbb7031d1300c1e4609e6b9e3acaaf2
kpc <kostroma.kpc@gmail.com>
2023-03-29 10:08:21 +0300

    Тестирование работы модели-представления главного окна

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CV19.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CV19.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel   
+    {
+        #region Заголовок окна
+        private string _Title = "Анализ статистики CV19";
+
+        public string Title
+        {
+            get => _Title;
+           /* set { 
+            
+                *//*if (Equals(_Title, value)) return;
+                _Title = value;
+                OnPropertyChanged();*//*
+
+                Set(ref _Title, value);
+            }*/
+            set => Set(ref _Title, value);          
+            
+        }
+
+        #endregion
+    }
+}
